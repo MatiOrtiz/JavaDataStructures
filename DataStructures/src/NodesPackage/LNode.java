938dd031531c189a117e9111e99b794bf8dbe695
MatiOrtiz <matiasnortiz.dev@gmail.com>
2023-04-29 18:03:28 -0300

    Node class for linkedlist added.

--- DataStructures/src/NodesPackage/LNode.java
+++ DataStructures/src/NodesPackage/LNode.java
@@ -4,10 +4,30 @@ import ADTList.Position;
 public class LNode<E> implements Position<E> {
 
 	protected E element;
-	protected LNode<E> node;
+	protected LNode<E> next;
 	
-	public LNode(E element, LNode<E> node) {
+	public LNode(E element, LNode<E> next) {
 		this.element= element;
-		this.node= node;
+		this.next= next;
 	}
+	
+	public LNode(E element) {
+		this(element, null);
+	}
+	
+	public void setElement(E element) {
+		this.element= element;
+	}
+	
+	public void setNext(LNode<E> next) {
+		this.next= next;
+	}
+	
+	public LNode<E> getNext() {
+		return next;
+	}
+	public E getElement() {
+		return element;
+	}
+	
 }
